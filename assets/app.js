d5b006f7e2806801b752f30025441b02473ecc0e
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-29 08:18:01 +0200

    dealing with modals that should be closed (including backdrops) before taking snapshot

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,40 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+import { Modal } from 'bootstrap';
+
+const findCacheControlMeta = () => {
+    return document.querySelector('meta[name="turbo-cache-control"]');
+}
+
+document.addEventListener('show.bs.modal', () => {
+    if (findCacheControlMeta()) {
+        return;
+    }
+
+    const meta = document.createElement('meta');
+    meta.name = 'turbo-cache-control';
+    meta.content = 'no-cache';
+    meta.dataset.removable = true;
+
+    document.querySelector('head').appendChild(meta);
+})
+
+document.addEventListener('hidden.bs.modal', () => {
+    const meta = findCacheControlMeta(); 
+    if (meta && meta.dataset.removable) {
+        meta.remove();
+    }
+})
+
+document.addEventListener('turbo:before-cache',()=>{
+    if (document.body.classList.contains('modal-open')) {
+        const modalElement = document.querySelector('.modal.show');
+        const modal = Modal.getInstance(modalElement);
+        modalElement.classList.remove('fade');
+        modal._backdrop._config.isAnimated = false;
+        modal.hide();
+        modal.dispose();
+    }
+})
