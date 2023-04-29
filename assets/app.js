c3742e3722963cb27bf1ef36f0c55cd3ea8c5aac
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-29 11:52:36 +0200

    refactor code from app.js to its own module

--- assets/app.js
+++ assets/app.js
@@ -11,25 +11,4 @@ import './styles/app.css';
 // start the Stimulus application
 import './bootstrap';
 
-import { Modal } from 'bootstrap';
-
-document.addEventListener('turbo:before-cache',()=>{
-    if (document.body.classList.contains('modal-open')) {
-        const modalElement = document.querySelector('.modal.show');
-        const modal = Modal.getInstance(modalElement);
-        modalElement.classList.remove('fade');
-        modal._backdrop._config.isAnimated = false;
-        modal.hide();
-        modal.dispose();
-    }
-
-    if (__webpack_modules__[require.resolveWeak('sweetalert2')]) {
-        // because we know it's been imported, this will run synchronously
-        import('sweetalert2').then((Swal) => {
-            if (Swal.default.isVisible()) {
-                Swal.default.getPopup().style.animationDuration = '0ms'
-                Swal.default.close();
-            }
-        })
-    }
-})
+import './turbo/turbo-helper';
--- assets/turbo/turbo-helper.js
+++ assets/turbo/turbo-helper.js
@@ -1,6 +1,42 @@
+import { Modal } from 'bootstrap';
+
+
+
 const TurboHelper = class {
     constructor() {
 
+        document.addEventListener('turbo:before-cache',()=>{
+            this.hideModal();
+            this.closePopup();
         
+
+        })
+
     }
+
+    hideModal() {
+        if (document.body.classList.contains('modal-open')) {
+            const modalElement = document.querySelector('.modal.show');
+            const modal = Modal.getInstance(modalElement);
+            modalElement.classList.remove('fade');
+            modal._backdrop._config.isAnimated = false;
+            modal.hide();
+            modal.dispose();
+        }
+    }    
+
+    closePopup() {
+        if (__webpack_modules__[require.resolveWeak('sweetalert2')]) {
+            // because we know it's been imported, this will run synchronously
+            import('sweetalert2').then((Swal) => {
+                if (Swal.default.isVisible()) {
+                    Swal.default.getPopup().style.animationDuration = '0ms'
+                    Swal.default.close();
+                }
+            })
+        }
+    }
+    
 } 
+
+export default new TurboHelper();
