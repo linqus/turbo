b656994e917a762c95e535c830930d34c0bf2403
Karol Linkiewicz <karol@linkiewicz.pl>
2023-09-18 12:52:07 +0200

    creating dedicated function for event, but doesn't work because of "this" is not bound

--- assets/controllers/modal-form_controller.js
+++ assets/controllers/modal-form_controller.js
@@ -1,30 +1,31 @@
 import { Controller } from 'stimulus';
 import { Modal } from 'bootstrap';
+import * as Turbo from '@hotwired/turbo';
 
 export default class extends Controller {
     static targets = ['modal'];
     modal = null;
 
     connect() {
-        this.element.addEventListener('turbo:before-fetch-response', (event) => {
-            console.log(event);
-
-            if (!this.modal || !this.modal._isShown) {
-                return;
-            }
-
-            const fetchResponse = event.detail.fetchResponse;
-
-            if (fetchResponse.succeeded && fetchResponse.redirected) {
-                event.preventDefault();
-                this.modal.hide();
-            }
-        });
+        this.element.addEventListener('turbo:before-fetch-response',this.beforeFetchResponse);
     }
 
     async openModal(event) {
         this.modal = new Modal(this.modalTarget);
         this.modal.show();
+    }
+
+    beforeFetchResponse(event) {
+        if (!this.modal || !this.modal._isShown) {
+            return;
+        }
+
+        const fetchResponse = event.detail.fetchResponse;
+
+        if (fetchResponse.succeeded && fetchResponse.redirected) {
+            event.preventDefault();
+            Turbo.visit(fetchResponse.location);
+        }
     }
 
 }
