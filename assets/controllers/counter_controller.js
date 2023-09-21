ebf5869bf3914c35af05498bbbb20ca5dba11304
Karol Linkiewicz <karol@linkiewicz.pl>
2023-09-21 17:44:42 +0200

    processing turbo-stream without calling backend

--- assets/controllers/counter_controller.js
+++ assets/controllers/counter_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from 'stimulus';
 // import * as Turbo from '@hotwired/turbo';
-import { visit } from '@hotwired/turbo'; // import only visit function
+import { visit, renderStreamMessage } from '@hotwired/turbo'; // import only visit function
 
 export default class extends Controller {
     count = 0;
@@ -9,6 +9,18 @@ export default class extends Controller {
     increment() {
         this.count++;
         this.countTarget.innerText = this.count;
+
+        const streamMessage = `
+        <turbo-stream action="update" target="flash-container">
+            <template>
+                <div class="alert alert-success">
+                    Thanks for clicking ${this.count} times!
+                </div>
+            </template>
+        </turbo-stream>
+        `; 
+
+        renderStreamMessage(streamMessage);
 
         if (this.count === 10) {
             // Turbo.visit('/you-won');
