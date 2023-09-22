143812e45d32487202c8f174f55ff5a77640835d
Karol Linkiewicz <karol@linkiewicz.pl>
2023-09-22 19:08:29 +0200

    create controller for toggling classes for new reviews, activate it and pass parameter to it

---
+++ assets/controllers/streamed-item_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from 'stimulus';
+
+export default class extends Controller {
+
+    static values = {
+        className: String,
+    }
+
+    connect() {
+        this.element.classList.add(this.classNameValue);
+
+        setTimeout( () => {
+            this.element.classList.add('fade-background');
+            this.element.classList.remove(this.classNameValue);
+        }, 500 )
+    }
+}
